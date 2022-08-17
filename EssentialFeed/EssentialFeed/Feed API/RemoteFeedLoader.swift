a25eda56543874aa986ffda6ba5b5fc7d16d634d
John Castillo <profe.johncastillo@gmail.com>
2022-08-17 09:15:41 -0500

    Move mapping logic to a new type FeedItemsMapper

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -5,60 +5,70 @@
 import Foundation
 
 public enum HTTPClientResult {
-	case success(Data, HTTPURLResponse)
-	case failure(Error)
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
 }
 
 public protocol HTTPClient {
-	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-	
-	public enum Result: Equatable {
-		case success([FeedItem])
-		case failure(Error)
-	}
-	
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (Result) -> Void) {
-		client.get(from: url) { result in
-			switch result {
-			case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map{$0.item}))
-				} else {
-					completion(.failure(.invalidData))
-				}
-			case .failure:
-				completion(.failure(.connectivity))
-			}
-		}
-	}
-}
-
-private struct Root: Decodable {
-	let items: [Item]
-}
-
-private struct Item: Decodable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let image: URL
+    private let url: URL
+    private let client: HTTPClient
     
-    var item: FeedItem {
-        return FeedItem(id: id, description: description, location: location, imageURL: image)
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public enum Result: Equatable {
+        case success([FeedItem])
+        case failure(Error)
+    }
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case let .success(data, response):
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
+                    completion(.failure(.invalidData))
+                }
+            case .failure:
+                completion(.failure(.connectivity))
+            }
+        }
+    }
+}
+
+private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+    
+    private struct Item: Decodable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return  try JSONDecoder().decode(Root.self, from: data).items.map{$0.item}
     }
 }
