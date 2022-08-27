bfde22b800d6419fc354f354799789abad0c3d73
John Castillo <profe.johncastillo@gmail.com>
2022-08-27 09:22:30 -0500

    Move the URLSessionHTTPClient (the system under test, or "SUT") creation to a factory method to protect our tests from breaking changes

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -24,8 +24,6 @@ class URLSessionHTTPClient {
             
         }.resume()
     }
-    
-    
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
@@ -38,7 +36,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.stopInterceptingRequest()
     }
     
-    func test_getFromURL_performsGETRequestWithURL() {        
+    func test_getFromURL_performsGETRequestWithURL() {
         let url:URL  = URL(string: "http://any-url.com")!
         let exp = expectation(description: "wait for request")
         
@@ -48,7 +46,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
     }
     
@@ -57,9 +55,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil ,error: error)
         
-        let sut = URLSessionHTTPClient()
         let exp = expectation(description: "Wait for completion")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -74,6 +71,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
